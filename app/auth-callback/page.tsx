f5aa97d6a65131ac14cc25a4f2193a40c27056ea
ChikenduHillary <chikenduhillchiagoziemu@gmail.com>
2024-05-18 02:29:12 +0100

    added suspence

--- app/auth-callback/page.tsx
+++ app/auth-callback/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Suspense } from "react"; // Step 1: Import Suspense
 import { useRouter, useSearchParams } from "next/navigation";
 import { trpc } from "../_trpc/client";
 import { Loader2 } from "lucide-react";
@@ -12,7 +13,7 @@ const Page = () => {
   const { data, isLoading } = trpc.authCallback.useQuery(undefined, {
     onSuccess: ({ success }) => {
       if (success) {
-        //user is siynced to db
+        //user is synced to db
         router.push(origin ? `/${origin}` : "/admin");
       }
     },
@@ -24,14 +25,19 @@ const Page = () => {
     retry: true,
     retryDelay: 500,
   });
+
   return (
-    <div className="w-full pt-24 text-center space-y-2 bg-background h-screen fixed top-0 justify-center">
-      <div className="flex flex-col items-center gap-2">
-        <Loader2 className="h-8 w-8 animate-spin text-zinc-800" />
-        <h3 className="font-semibold text-xl">Setting up your account...</h3>
+    <Suspense fallback={<div>Loading...</div>}>
+      {" "}
+      {/* Step 2: Wrap useSearchParams with Suspense */}
+      <div className="w-full pt-24 text-center space-y-2 bg-background h-screen fixed top-0 justify-center">
+        <div className="flex flex-col items-center gap-2">
+          <Loader2 className="h-8 w-8 animate-spin text-zinc-800" />
+          <h3 className="font-semibold text-xl">Setting up your account...</h3>
+        </div>
+        <p>You will be redirected automatically.</p>
       </div>
-      <p>You will be redirected automatically.</p>
-    </div>
+    </Suspense>
   );
 };
 
