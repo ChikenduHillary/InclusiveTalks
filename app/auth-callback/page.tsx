2397b61a0f38fe37f6737ab02c181b598e8d4158
ChikenduHillary <chikenduhillchiagoziemu@gmail.com>
2024-05-18 02:43:07 +0100

    added suspence

--- app/auth-callback/page.tsx
+++ app/auth-callback/page.tsx
@@ -10,6 +10,15 @@ const Page = () => {
   const searchParams = useSearchParams();
   const origin = searchParams.get("origin");
 
+  // Wrap the section that depends on the search parameters with Suspense
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <PageContent origin={origin} router={router} />
+    </Suspense>
+  );
+};
+
+const PageContent = ({ origin, router }: any) => {
   const { data, isLoading } = trpc.authCallback.useQuery(undefined, {
     onSuccess: ({ success }) => {
       if (success) {
@@ -27,15 +36,13 @@ const Page = () => {
   });
 
   return (
-    <Suspense>
-      <div className="w-full pt-24 text-center space-y-2 bg-background h-screen fixed top-0 justify-center">
-        <div className="flex flex-col items-center gap-2">
-          <Loader2 className="h-8 w-8 animate-spin text-zinc-800" />
-          <h3 className="font-semibold text-xl">Setting up your account...</h3>
-        </div>
-        <p>You will be redirected automatically.</p>
+    <div className="w-full pt-24 text-center space-y-2 bg-background h-screen fixed top-0 justify-center">
+      <div className="flex flex-col items-center gap-2">
+        <Loader2 className="h-8 w-8 animate-spin text-zinc-800" />
+        <h3 className="font-semibold text-xl">Setting up your account...</h3>
       </div>
-    </Suspense>
+      <p>You will be redirected automatically.</p>
+    </div>
   );
 };
 
