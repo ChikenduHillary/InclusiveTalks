f42eb82303197c7cb02462d939a2c29629a0d777
ChikenduHillary <chikenduhillchiagoziemu@gmail.com>
2024-07-19 17:00:51 +0100

    made some modification

--- app/auth-callback/page.tsx
+++ app/auth-callback/page.tsx
@@ -17,12 +17,18 @@ const Page = () => {
   );
 };
 
-const PageContent = ({ origin, router }: any) => {
+const PageContent = ({
+  origin,
+  router,
+}: {
+  origin: string | null;
+  router: any;
+}) => {
   const { data, isLoading } = trpc.authCallback.useQuery(undefined, {
     onSuccess: ({ success }) => {
       console.log({ success });
       if (success) {
-        //user is synced to db
+        // User is synced to db
         router.push(origin ? `/${origin}` : "/admin");
       }
     },
@@ -32,15 +38,19 @@ const PageContent = ({ origin, router }: any) => {
     },
   });
 
-  return (
-    <div className="w-full pt-24 text-center space-y-2 bg-background h-screen fixed top-0 justify-center">
-      <div className="flex flex-col items-center gap-2">
-        <Loader2 className="h-8 w-8 animate-spin text-zinc-800" />
-        <h3 className="font-semibold text-xl">Setting up your account...</h3>
+  if (isLoading) {
+    return (
+      <div className="w-full pt-24 text-center space-y-2 bg-background h-screen fixed top-0 justify-center">
+        <div className="flex flex-col items-center gap-2">
+          <Loader2 className="h-8 w-8 animate-spin text-zinc-800" />
+          <h3 className="font-semibold text-xl">Setting up your account...</h3>
+        </div>
+        <p>You will be redirected automatically.</p>
       </div>
-      <p>You will be redirected automatically.</p>
-    </div>
-  );
+    );
+  }
+
+  return null; // Or render something else when not loading
 };
 
 export default Page;
